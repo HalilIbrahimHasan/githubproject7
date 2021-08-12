70a430f8a90e4f241d7bf2bf1eed7f6e5cf7ef78
Ibrahim Kalin <hisetkal@gmail.com>
2021-08-12 18:17:07 -0400

    first commit from ibrahim

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -3,6 +3,8 @@ package package1;
 public class ClassOne {
 
 	public static void main(String[] args) {
+		System.out.println("Here is master branch");
 		System.out.println("Hello world");
+		System.out.println("Hello today");
 	}
 }
